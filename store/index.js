73286f0497ce850210cf97208b324be50d026a81
cedrictaro-g <cedric.tarou@gmail.com>
2022-01-09 14:57:51 +0900

    Edit Home Tooblar & App Menu Component

---
+++ store/index.js
@@ -0,0 +1,10 @@
+export const state = () => ({
+  styles: {
+    homeAppBar: {
+      homeAppBarHeight: 56,
+    },
+  },
+})
+export const gatters = {}
+export const mutations = {}
+export const actions = {}
