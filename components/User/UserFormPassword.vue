65cb5c69e33a36bace89bdcc454e71236913ee8c
cedrictaro-g <cedric.tarou@gmail.com>
2022-01-09 20:49:34 +0900

    Set signup form validation

--- components/User/UserFormPassword.vue
+++ components/User/UserFormPassword.vue
@@ -1,18 +1,35 @@
 <template>
   <v-text-field
     v-model="setPassword"
+    :rules="form.rules"
+    :counter="setValidation"
+    :hint="form.hint"
     label="パスワードを入力"
-    placeholder="8文字以上"
+    :placeholder="form.placeholder"
+    :hide-details="!setValidation"
     outlined
+    :append-icon="toggle.icon"
+    :type="toggle.type"
+    autocomplete="on"
+    @click:append="show = !show"
   />
 </template>
 <script>
 export default {
-  prpos: {
+  props: {
     password: {
       type: String,
       default: '',
     },
+    setValidation: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      show: false,
+    }
   },
   computed: {
     setPassword: {
@@ -22,6 +39,22 @@ export default {
       set(newValue) {
         return this.$emit('update:password', newValue)
       },
+    },
+    form() {
+      const min = '8文字以上'
+      const msg = `${min}。半角英数字•ﾊｲﾌﾝ•ｱﾝﾀﾞｰﾊﾞｰが使えます`
+      const required = (v) => !!v || ''
+      const format = (v) => /^[\w-]{8,72}$/.test(v) || msg
+
+      const rules = this.setValidation ? [format] : [required]
+      const hint = this.setValidation ? msg : undefined
+      const placeholder = this.setValidation ? min : undefined
+      return { rules, hint, placeholder }
+    },
+    toggle() {
+      const icon = this.show ? 'mdi-eye' : 'mdi-eye-off'
+      const type = this.show ? 'text' : 'password'
+      return { icon, type }
     },
   },
 }
